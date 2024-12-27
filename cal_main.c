a1b4a9ed6ff63c1e7a1c52368631e78814e6aea1
rindongchoi <cdr440@naver.com>
2024-12-27 15:36:18 +0900

    change cal_main.c

--- cal_main.c
+++ cal_main.c
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     int a = atoi(argv[1]);
     int b = atoi(argv[2]);
 
+    printf("--------------")
     printf("%d + %d = %d\n", a, b, add(a , b));
     printf("%d - %d = %d\n", a, b, sub(a , b));
     printf("%d * %d = %d\n", a, b, mul(a , b));
